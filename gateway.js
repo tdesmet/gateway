6ef590541c69772278c10418e3b6c23eefa41d79
Tom De Smet <tdesmet@gmail.com>
2019-01-02 09:31:12 +0100

    Add better error handling and logging

--- gateway.js
+++ gateway.js
@@ -9,7 +9,23 @@ const cfg = require('home-config').load('.lxgw', {
 const { Observable, Subject, ReplaySubject, from, of, range } = require('rxjs');
 const { map, filter, switchMap } = require('rxjs/operators');
 const Loxone = require('./loxone');
-
+const SimpleLogger = require('simple-node-logger');
+const serializeError = require('serialize-error');
+const manager = new SimpleLogger();
+manager.createConsoleAppender();
+const log = manager.createLogger('Gateway', 'info');
+process.on('error', (msg) => {
+    log.error('Error event caught: ', serializeError(msg));
+});
+console.error = function(...args) {
+  log.error(...args);
+};
+console.warn = function(...args) {
+  log.warn(...args);
+};
+console.info = function(...args) {
+  log.info(...args);
+};
 const uuidPerZone = {
   "woonkamer": "12ce8849-0036-7239-ffff735f95f7f110",
   "Badkamer": "12ce8e8c-0236-0490-ffff735f95f7f110",
@@ -24,61 +40,96 @@ module.exports = function() {
   let homeId;
   let zones;
   const tado = new Tado();
-  const loxone = new Loxone(cfg.loxone);
+  const loxone = new Loxone(cfg.loxone, manager.createLogger('Loxone', 'info'));
 
-  async function setup() {
-    // Login to the Tado Web API
-    await tado.login(cfg.tado.username, cfg.tado.password);
+  function setup() {
+    setupTado();
+    loxone.connect();
+  }
 
-    // Get the User's information
-    const me = await tado.getMe();
-
-    if (!me.homes || me.homes.length === 0) {
-      console.error("No homes found");
-      process.exitCode = 1;
+  async function setupTado() {
+    try {
+      log.info("Trying to login on tado");
+      // Login to the Tado Web API
+      await tado.login(cfg.tado.username, cfg.tado.password);
+    } catch(err) {
+      log.error("Failed to login to tado ", serializeError(err));
+      setTimeout(setupTado, 10000);
       return;
     }
+    // Get the User's information
+    try {
+      log.info("Trying to get me from tado");
+      const me = await tado.getMe();
 
-    homeId = me.homes[0].id;
-    await loxone.connect();
-    zones = (await tado.getZones(homeId)).filter(z => z.type === "HEATING");
-    console.log(zones);
-    await pollDevices();
+      if (!me.homes || me.homes.length === 0) {
+        log.error("No homes found");
+        setTimeout(setupTado, 10000);
+        return;
+      }
+
+      homeId = me.homes[0].id;
+    } catch(err) {
+      log.error("Failed to get me from tado ", serializeError(err));
+      setTimeout(setupTado, 10000);
+      return;
+    }
+    try {
+      log.info("Trying to get the zones from tado");
+      zones = (await tado.getZones(homeId)).filter(z => z.type === "HEATING");
+    } catch (err) {
+      log.error("Failed to get the zones from tado ", serializeError(err));
+      setTimeout(setupTado, 10000);
+      return;
+    }
+    pollDevices();
+    pollZones();
   }
 
   async function pollDevices() {
-    const devices = await tado.getMobileDevices(homeId);
-    let atHome = false;
-    for (const device of devices) {
-      if (device.location) {
-        atHome = atHome || device.location.atHome;
+    try {
+      const devices = await tado.getMobileDevices(homeId);
+      let atHome = false;
+      for (const device of devices) {
+        if (device.location) {
+          atHome = atHome || device.location.atHome;
+        }
       }
-    }
 
-    if (atleastOneAtHome !== atHome) {
-      athomeSubject.next(atHome);
-    }
-    atleastOneAtHome = atHome;
-    for (const zone of zones) {
-      const zoneState = await tado.getZoneState(homeId, zone.id);
-      if (uuidPerZone[zone.name]) {
-        await loxone.sendCommand(uuidPerZone[zone.name], zoneState.sensorDataPoints.insideTemperature.celsius);
+      if (atleastOneAtHome !== atHome) {
+        athomeSubject.next(atHome);
       }
+      atleastOneAtHome = atHome;
+    } catch (err) {
+      log.error("Failed to get the mobile devices from tado", serializeError(err));
     }
-
     setTimeout(() => {
       pollDevices();
     }, 15000);
   }
 
+  async function pollZones() {
+    try {
+      for (const zone of zones) {
+        const zoneState = await tado.getZoneState(homeId, zone.id);
+        if (uuidPerZone[zone.name]) {
+          await loxone.sendCommand(uuidPerZone[zone.name], zoneState.sensorDataPoints.insideTemperature.celsius);
+        }
+      }
+    } catch (err) {
+      log.error("Failed to get the zone status from tado", serializeError(err));
+    }
+    setTimeout(() => {
+      pollZones();
+    }, 60000);
+  }
+
   atHomeChangedObservable.subscribe(async (atHome) => {
     if (atHome) {
-      console.log('atHome');
-
+      log.info("Someone arrived home");
       await loxone.sendCommand('12ce6b18-0277-2dcc-ffffec7387844f58', 'pulse');
     } else {
-      console.log('notAtHome');
-
+      log.info("Everyone has left home");
       await loxone.sendCommand('12ce69d5-00d9-2471-ffff255dddd2c9d4', 'pulse');
     }
   });
--- loxone.js
+++ loxone.js
@@ -1,26 +1,33 @@
 const LxCommunicator = require('lxcommunicator');
+const WebSocketConfig = LxCommunicator.WebSocketConfig;
 const uuidv4 = require('uuid/v4');
+const serializeError = require('serialize-error');
 
 module.exports = class Loxone {
-  constructor(config) {
+  constructor(config, log) {
     this.connected = false;
     this.uuid = uuidv4();
     this.deviceInfo = require('os').hostname();
     this.config = config;
+    this.log = log;
     this.cache = [];
-    this.delegate = {
+    // Instantiate a config object to pass it to the LxCommunicator.WebSocket later
+    const webSocketConfig = new WebSocketConfig(WebSocketConfig.protocol.WS, this.uuid, this.deviceInfo, WebSocketConfig.permission.APP, false);
+    // OPTIONAL: assign the delegateObj to be able to react on delegate calls
+    webSocketConfig.delegate = {
       socketOnDataProgress: (socket, progress) => {
-        console.log(progress);
+        this.log.info("data progress ", progress);
       },
       socketOnTokenConfirmed: (socket, response) => {
-        console.log(response);
+        this.log.info("token confirmed ", serializeError(response));
       },
       socketOnTokenReceived: (socket, result) => {
-        console.log(result);
+        this.log.info("token received ", serializeError(result));
       },
       socketOnConnectionClosed: (socket, code) => {
-        console.log(code);
+        this.log.info("Socket closed ", code);
         this.connected = false;
+        this.log.info("Trying to reconnect");
         this.connect();
       },
       socketOnEventReceived: (socket, events, type) => {
@@ -30,34 +37,61 @@ module.exports = class Loxone {
           }
         }
       }
-    }
-  }
-  async connect() {
-    const WebSocketConfig = LxCommunicator.WebSocketConfig;
-
-    // Instantiate a config object to pass it to the LxCommunicator.WebSocket later
-    const config = new WebSocketConfig(WebSocketConfig.protocol.WS, this.uuid, this.deviceInfo, WebSocketConfig.permission.APP, false);
-    // OPTIONAL: assign the delegateObj to be able to react on delegate calls
-    config.delegate = this.delegate;
-
+    };
     // Instantiate the LxCommunicator.WebSocket, it is our actual WebSocket
-    this.socket = new LxCommunicator.WebSocket(config);
+    this.socket = new LxCommunicator.WebSocket(webSocketConfig);
+  }
+
+  close() {
+    try {
+    this.socket.close();
+    } catch (err) {
+
+    }
+    this.connected = false;
+  }
+
+  async connect() {
+    this.log.info("Trying to connect");
+    if (this.connected) {
+      this.log.info("We are connected, ignoring");
+      return;
+    }
 
     try {
       // Open a Websocket connection to a miniserver by just providing the host, username and password!
       await this.socket.open(this.config.address, this.config.username, this.config.password);
     } catch(error) {
+      this.log.warn("Failed to connect, retrying in 10 seconds");
       setTimeout(() => {
         this.connect();
       }, 10000);
+      return;
     }
     //await this.socket.send("jdev/sps/enablebinstatusupdate");
-    const file = await this.socket.send("data/LoxAPP3.json");
-    console.log(file);
+    try {
+      this.log.info("Requesting structure file");
+      const file = await this.socket.send("data/LoxAPP3.json");
+    } catch (error) {
+      this.log.error("Failed to get structure file ", serializeError(error));
+      this.close();
+      setTimeout(() => {
+        this.connect();
+      }, 5000);
+      return;
+    }
     this.connected = true;
   }
 
   async sendCommand(uuid, command) {
-    await this.socket.send(`jdev/sps/io/${uuid}/${command}`);
+    if (!this.connected) {
+      this.log.warn("Not connected, Ignoring command", serializeError({uuid: uuid, command: command}));
+      return;
+    }
+    try {
+      await this.socket.send(`jdev/sps/io/${uuid}/${command}`);
+    } catch(err) {
+      this.log.error("Failed to send command ", serializeError({err: err, uuid: uuid, command: command}));
+    }
   }
 }
